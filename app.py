0895c9b3fedc33ab36361a79f8b4f611ae0a5b6d
ikeoffiah <pispio12@gmail.com>
2021-09-15 23:19:52 -0700

    I added benjamin dada tech news site

--- app.py
+++ app.py
@@ -13,23 +13,26 @@ def home():
     tech_point = requests.get("https://techpoint.africa/").text
     techcabal = requests.get("https://techcabal.com/category/startups/").text
     disrupt = requests.get("https://disrupt-africa.com/").text
+    bd = requests.get("https://www.benjamindada.com/").text
 
     soup_point = BeautifulSoup(tech_point, 'lxml')
     soup_cabal = BeautifulSoup(techcabal, 'lxml')
     soup_disrupt = BeautifulSoup(disrupt, 'lxml')
+    soup_bd = BeautifulSoup(bd, 'lxml')
 
     point = soup_point.find_all('h4', class_="entry-title")
     cabal = soup_cabal.find_all('a', class_="article-list-title")
     rupt = soup_disrupt.find_all('a', class_="post-title")
+    ben = soup_bd.find_all('a', class_="post-card-content-link")
 
 
     news_point = [{'news':n.find('a').text,'link':n.find('a')['href']} for n in point]
-    news_cabal = [{'news':n.text.strip(),'link':n["href"].strip()} for n in cabal]
+    news_cabal = [{'news':n.text.strip(),'link':n["href"].strip()} for n in cabal if n.text !=""]
     news_rupt = [{'news': n.text.strip(), 'link': n["href"].strip()} for n in rupt]
+    news_bd = [{'news':r.h2.text,'link':f'https://www.benjamindada.com{r["href"]}'} for r in ben if r.find('span',class_="post-card-tags")]
 
 
-
-    return render_template('news.html',news_rupt=news_rupt, news_cabal=news_cabal, news_point=news_point)
+    return render_template('news.html',news_rupt=news_rupt, news_cabal=news_cabal, news_point=news_point, news_bd=news_bd)
 
 
 
